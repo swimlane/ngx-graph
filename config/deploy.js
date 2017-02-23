07fa0d4d102ed6c929d665e2ba0ed88e258f04cd
Marjan Georgiev <marjan.georgiev@gmail.com>
2017-02-23 18:20:45 +0100

    Update config

--- config/deploy.js
+++ config/deploy.js
@@ -4,8 +4,8 @@ var ghpages = require('gh-pages');
 var dir = path.resolve(path.join(__dirname, '../', 'dist'));
 ghpages.publish(dir, {
   user: {
-    name: 'Austin McDaniel',
-    email: 'amcdaniel2@gmail.com'
+    name: 'Marjan Georgiev',
+    email: 'marjan.georgiev@gmail.com'
   },
   message: '(deploy): CI',
   logger: function(message) {
