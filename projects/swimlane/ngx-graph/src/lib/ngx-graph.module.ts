95aa651dc92d31db67f3c3a6b24165dcefd1a509
Marjan Georgiev <marjan.georgiev@gmail.com>
2019-03-11 21:28:13 +0100

    Export the graph component from the module

--- projects/swimlane/ngx-graph/src/lib/ngx-graph.module.ts
+++ projects/swimlane/ngx-graph/src/lib/ngx-graph.module.ts
@@ -3,6 +3,7 @@ import { GraphModule } from './graph/graph.module';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 export * from './models/index';
+export * from './graph/graph.component';
 
 @NgModule({
   imports: [NgxChartsModule],
