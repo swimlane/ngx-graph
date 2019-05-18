110f0db806dc453791381d94f854b78d26c39d41
assafsun <assaf.sun@gmil.com>
2019-05-18 12:52:06 +0300

    Adding a custom curve demo + create demo components folder

--- src/docs/demos/demo.module.ts
+++ src/docs/demos/demo.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BaseModule } from './base/base.module';
+import { NgxGraphCustomCurve } from './components/ngx-graph-custom-curve/ngx-graph-custom-curve.component';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
 
 @NgModule({
-  imports: [BaseModule]
+  imports: [BaseModule,
+    NgxGraphModule
+  ],
+  declarations: [NgxGraphCustomCurve],
+  exports: [
+    NgxGraphCustomCurve
+  ],
 })
 export class DemoModule {}
