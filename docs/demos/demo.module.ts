dbf66eef8129e2553c2601aefc6132d4f2cdb5b1
Marjan Georgiev <marjan.georgiev@gmail.com>
2019-06-21 17:19:48 +0200

    Auto-generated commit

--- docs/demos/demo.module.ts
+++ docs/demos/demo.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BaseModule } from './base/base.module';
+import { CommonModule } from '@angular/common';
+import { NgxGraphCustomCurve } from './components/ngx-graph-custom-curve/ngx-graph-custom-curve.component';
+import { NgxGraphOrgTreeComponent } from './components/ngx-graph-org-tree/ngx-graph-org-tree.component';
+import { NgxGraphModule } from '@swimlane/ngx-graph';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
-  imports: [BaseModule]
+  imports: [BaseModule,
+    CommonModule,
+    NgxGraphModule,
+    NgxChartsModule
+  ],
+  declarations: [
+    NgxGraphCustomCurve,
+    NgxGraphOrgTreeComponent
+  ],
+  exports: [
+    NgxGraphCustomCurve,
+    NgxGraphOrgTreeComponent
+  ],
 })
 export class DemoModule {}
