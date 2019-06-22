37b9aee127dff90b789f5df4c295654caad34a6a
KingDarBoja <mbojato@uninorte.edu.co>
2019-06-22 14:44:08 -0500

    Removed references to ngx-charts

--- src/docs/demos/demo.module.ts
+++ src/docs/demos/demo.module.ts
@@ -4,13 +4,11 @@ import { CommonModule } from '@angular/common';
 import { NgxGraphCustomCurve } from './components/ngx-graph-custom-curve/ngx-graph-custom-curve.component';
 import { NgxGraphOrgTreeComponent } from './components/ngx-graph-org-tree/ngx-graph-org-tree.component';
 import { NgxGraphModule } from '@swimlane/ngx-graph';
-import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @NgModule({
   imports: [BaseModule,
     CommonModule,
     NgxGraphModule,
-    NgxChartsModule
   ],
   declarations: [
     NgxGraphCustomCurve,
