8cfac6e625b9ce4c49f1f0a874c75df8ac05f430
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2021-01-21 10:40:15 -0700

    use new polyfill to support safari

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -74,12 +74,7 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-// Used for browsers with partially native support of Custom Elements
-import '@webcomponents/custom-elements/src/native-shim';
-
-// Used for browsers without a native support of Custom Elements
-import '@webcomponents/custom-elements/custom-elements.min';
-
+import '@ungap/custom-elements';
 import '@ungap/global-this';
 
 window['global'] = globalThis as any;
