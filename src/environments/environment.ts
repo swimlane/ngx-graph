053071cc75cb8659e378977e9f8d463ae685cbd7
Marjan Georgiev <marjan.georgiev@gmail.com>
2021-10-20 16:07:26 +0200

    Upgrade to ng12

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
-import { version } from '../../projects/swimlane/ngx-graph/package.json';
+import packageInfo from '../../projects/swimlane/ngx-graph/package.json';
 
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
@@ -6,7 +6,7 @@ import { version } from '../../projects/swimlane/ngx-graph/package.json';
 
 export const environment = {
   production: false,
-  version
+  version: packageInfo.version
 };
 
 /*
@@ -15,4 +15,4 @@ export const environment = {
  * import the following file, but please comment it out in production mode
  * because it will have performance impact when throw error
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
