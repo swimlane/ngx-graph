ab66f36d51a887d7746612dda64d58dead16571f
steveblue <steveblue@users.noreply.github.com>
2024-11-04 16:38:49 +0000

    Deploying to gh-pages from @ swimlane/ngx-graph@9944495478eff9a1da30395ae6b607e7836258a0 🚀

--- docs/demos/base/base.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { NgxGraphModule } from '@swimlane/ngx-graph';
-import { CommonModule } from '@angular/common';
-
-@NgModule({
-  imports: [NgxGraphModule, CommonModule]
-})
-export class BaseModule {}
