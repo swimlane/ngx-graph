bcaed71e5f89725fe106c0a4190d63657ca25e15
steveblue <steveblue@users.noreply.github.com>
2026-05-14 19:49:20 +0000

    Deploying to gh-pages from @ swimlane/ngx-graph@7dcd0207f34fdc0c4be6dd641442943fb2a47ef7 🚀

--- sb-preview/globals.js
+++ sb-preview/globals.js
@@ -1,33 +1,20 @@
-import ESM_COMPAT_Module from "node:module";
-import { fileURLToPath as ESM_COMPAT_fileURLToPath } from 'node:url';
-import { dirname as ESM_COMPAT_dirname } from 'node:path';
-const __filename = ESM_COMPAT_fileURLToPath(import.meta.url);
-const __dirname = ESM_COMPAT_dirname(__filename);
-const require = ESM_COMPAT_Module.createRequire(import.meta.url);
+import "../_browser-chunks/chunk-A242L54C.js";
 
 // src/preview/globals/globals.ts
-var _ = {
+var globalsNameReferenceMap = {
   "@storybook/global": "__STORYBOOK_MODULE_GLOBAL__",
+  "storybook/test": "__STORYBOOK_MODULE_TEST__",
+  "storybook/actions": "__STORYBOOK_MODULE_ACTIONS__",
+  "storybook/preview-api": "__STORYBOOK_MODULE_PREVIEW_API__",
   "storybook/internal/channels": "__STORYBOOK_MODULE_CHANNELS__",
-  "@storybook/channels": "__STORYBOOK_MODULE_CHANNELS__",
-  "@storybook/core/channels": "__STORYBOOK_MODULE_CHANNELS__",
   "storybook/internal/client-logger": "__STORYBOOK_MODULE_CLIENT_LOGGER__",
-  "@storybook/client-logger": "__STORYBOOK_MODULE_CLIENT_LOGGER__",
-  "@storybook/core/client-logger": "__STORYBOOK_MODULE_CLIENT_LOGGER__",
   "storybook/internal/core-events": "__STORYBOOK_MODULE_CORE_EVENTS__",
-  "@storybook/core-events": "__STORYBOOK_MODULE_CORE_EVENTS__",
-  "@storybook/core/core-events": "__STORYBOOK_MODULE_CORE_EVENTS__",
   "storybook/internal/preview-errors": "__STORYBOOK_MODULE_CORE_EVENTS_PREVIEW_ERRORS__",
-  "@storybook/core-events/preview-errors": "__STORYBOOK_MODULE_CORE_EVENTS_PREVIEW_ERRORS__",
-  "@storybook/core/preview-errors": "__STORYBOOK_MODULE_CORE_EVENTS_PREVIEW_ERRORS__",
-  "storybook/internal/preview-api": "__STORYBOOK_MODULE_PREVIEW_API__",
-  "@storybook/preview-api": "__STORYBOOK_MODULE_PREVIEW_API__",
-  "@storybook/core/preview-api": "__STORYBOOK_MODULE_PREVIEW_API__",
   "storybook/internal/types": "__STORYBOOK_MODULE_TYPES__",
-  "@storybook/types": "__STORYBOOK_MODULE_TYPES__",
-  "@storybook/core/types": "__STORYBOOK_MODULE_TYPES__"
-}, O = Object.keys(_);
+  // @deprecated TODO: Remove in 9.1 or some point in the future, I guess
+  "storybook/internal/preview-api": "__STORYBOOK_MODULE_PREVIEW_API__"
+}, globalPackages = Object.keys(globalsNameReferenceMap);
 export {
-  O as globalPackages,
-  _ as globalsNameReferenceMap
+  globalPackages,
+  globalsNameReferenceMap
 };
